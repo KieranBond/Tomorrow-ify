885847cb79330b5c49ac30ce6ced875ca9281332
Kieran Bond <kieran.bond@iress.com>
2024-02-17 16:25:35 +0000

    Add dynamic elements to website

--- src/Frontend/TomorrowifyLogin.js
+++ src/Frontend/TomorrowifyLogin.js
@@ -5,7 +5,6 @@ function signUp()
 {
     console.log("Hallo World");
     SpotifyAuthRedirect();
-
 }
 
 // Redirect the user to Spotify authorization endpoint
@@ -14,7 +13,7 @@ function SpotifyAuthRedirect() {
 
     //Get the current URL
     var redirect_uri = window.location.href;
-    console.log("Offering redirect to: " + redirect_uri);
+    console.log('Offering redirect to: ' + redirect_uri);
     var scope = 'playlist-modify-public playlist-modify-private playlist-read-private';//Probably need more permissions e.g. 
 
     var url = 'https://accounts.spotify.com/authorize';
@@ -26,6 +25,22 @@ function SpotifyAuthRedirect() {
     // Redirect the user to Spotify authorization endpoint
     window.location.href = url;
 }
+
+function AddSignUpButton() {
+    const button = document.createElement("button");
+    button.innerHTML = "Sign up to Tomorrow-ify";
+    button.onclick = signUp;
+    document.getElementById("Signup").appendChild(button);
+}
+
+function AddSignedUpText() {
+    const textElement = document.createElement("p");
+    const textNode = document.createTextNode("You're all signed up!");
+    textElement.appendChild(textNode);
+    
+    document.getElementById("Signup").appendChild(textElement);
+}
+
 // Parse the authorization code from the URL
 window.onload = function() {
     // Parse the URL query string
@@ -40,7 +55,11 @@ window.onload = function() {
         // Call to server function to swap authorization code for access token
         // This needs to be done server-side because it requires the client secret
 
+        AddSignedUpText();
+
     } else {
         console.log('Authorization code not found.');
+
+        AddSignUpButton();
     }
 };
