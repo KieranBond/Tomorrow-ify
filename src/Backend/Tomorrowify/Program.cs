b720437b04d9733576ce6e1a7c47b4238e0af606
Ashley Cheema <ashleyycheemaa@hotmail.com>
2024-02-21 21:31:20 +0000

    add/playlist updater/empty tomorrow (#14)
    Don't replace or add to the Today playlist if the Tomorrow playlist is empty
    
    Authored by @ashleycheema

--- src/Backend/Tomorrowify/Program.cs
+++ src/Backend/Tomorrowify/Program.cs
@@ -58,17 +58,20 @@ app.MapPost("/{token}", async (string token, Configuration configuration) =>
         .Select(t => t.Track as FullTrack)
         .Where(t => t?.Id != null);
 
-    await spotify.Playlists.ReplaceItems(todayPlaylist.Id!, 
-        new PlaylistReplaceItemsRequest(tomorrowTracks.Take(100).Select(t => t!.Uri).ToList())
-    );
+    if (!tomorrowTracks.Any())
+        return Results.Ok();
 
-    if(tomorrowTracks.Count() > 100)
+     await spotify.Playlists.ReplaceItems(todayPlaylist.Id!,
+         new PlaylistReplaceItemsRequest(tomorrowTracks.Take(100).Select(t => t!.Uri).ToList())
+     );
+
+    if (tomorrowTracks.Count() > 100)
     {
         var remainingTracks = tomorrowTracks.Skip(100);
         var tracksToAdd = remainingTracks.Chunk(100);
-        foreach(var chunk in tracksToAdd)
+        foreach (var chunk in tracksToAdd)
         {
-            await spotify.Playlists.AddItems(tomorrowPlaylist.Id!, 
+            await spotify.Playlists.AddItems(tomorrowPlaylist.Id!,
                 new PlaylistAddItemsRequest(chunk.Select(t => t!.Uri).ToList())
             );
         }
